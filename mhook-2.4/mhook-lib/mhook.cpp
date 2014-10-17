9bf609b25ba2e86276a53d256c6e77f96d31e0eb
houbaoshan <houbaoshan@iwallee.com>
2014-10-17 20:17:17 +0800

    修复挂起线程可能导致内存分配停滞的问题

--- mhook-2.4/mhook-lib/mhook.cpp
+++ mhook-2.4/mhook-lib/mhook.cpp
@@ -52,47 +52,22 @@ inline void __cdecl odprintf(PCSTR format, ...)
 {
     va_list	args;
     va_start(args, format);
-    int len = _vscprintf(format, args);
-    if (len > 0) {
-        len += (1 + 2);
-        PSTR buf = (PSTR) malloc(len);
-        if (buf) {
-            len = vsprintf_s(buf, len, format, args);
-            if (len > 0) {
-                while (len && isspace(buf[len-1])) len--;
-                buf[len++] = '\r';
-                buf[len++] = '\n';
-                buf[len] = 0;
-                OutputDebugStringA(buf);
-                printf(buf);
-            }
-            free(buf);
-        }
-        va_end(args);
-    }
+    char buf[1024] = {0};
+    vsprintf_s(buf, _countof(buf), format, args);
+    OutputDebugStringA(buf);
+    printf(buf);
+    va_end(args);
 }
 
 inline void __cdecl odprintf(PCWSTR format, ...)
 {
     va_list	args;
     va_start(args, format);
-    int len = _vscwprintf(format, args);
-    if (len > 0) {
-        len += (1 + 2);
-        PWSTR buf = (PWSTR) malloc(sizeof(WCHAR)*len);
-        if (buf) {
-            len = vswprintf_s(buf, len, format, args);
-            if (len > 0) {
-                while (len && iswspace(buf[len-1])) len--;
-                buf[len++] = L'\r';
-                buf[len++] = L'\n';
-                buf[len] = 0;
-                OutputDebugStringW(buf);
-            }
-            free(buf);
-        }
-        va_end(args);
-    }
+    wchar_t buf[1024] = { 0 };
+    vswprintf_s(buf, _countof(buf), format, args);
+    OutputDebugStringW(buf);
+    wprintf(buf);
+    va_end(args);
 }
 
 #endif //#ifndef ODPRINTF
