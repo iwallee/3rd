5fcde351aa287a1be964687489ae464073e1ca5f
houbaoshan <houbaoshan@iwallee.com>
2015-02-03 16:18:27 +0800

    cairo dll 1.14

--- cairo/build-release.sh
+++ cairo/build-release.sh
@@ -3,11 +3,11 @@
 # cp "libpng1616/projects/vstudio/Release Library/libpng16.lib" "libpng1616/libpng.lib" 
 # cp "zlib-1.2.8/build/Release/zlibstatic.lib" "zlib-1.2.8/zdll.lib"
  
-set CAIROROOT=%cd%
+set CAIROROOT=C:/Develop/code/iwallee/teedow/3rd/cairo
 set "INCLUDE=%INCLUDE%;%CAIROROOT%/src;%CAIROROOT%/pixman/pixman;%CAIROROOT%/zlib-1.2.8;%CAIROROOT%/libpng1616;"
 
 cd pixman
 make -f Makefile.win32 "CFG=release" "MMX=on" "SSE2=on" "SSSE3=on" 
 
 cd ..
-make -f Makefile.win32 "CFG=release" "LIBPNG_PATH=%CAIROROOT%/libpng1616" "ZLIB_PATH=%CAIROROOT%/zlib-1.2.8" "PIXMAN_PATH=%CAIROROOT%/pixman"
+make -f Makefile.win32 "CFG=release" "LIBPNG_PATH=C:/Develop/code/iwallee/teedow/3rd/cairo/libpng1616" "ZLIB_PATH=C:/Develop/code/iwallee/teedow/3rd/cairo/zlib-1.2.8" "PIXMAN_PATH=C:/Develop/code/iwallee/teedow/3rd/cairo/pixman"
