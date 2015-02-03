2c13a0ae354062ad36b20db430d5c8b3441876d5
houbaoshan <houbaoshan@iwallee.com>
2015-02-03 15:37:23 +0800

    cairo

---
+++ cairo/build-debug.sh
@@ -0,0 +1,13 @@
+rem rm "libpng1616/libpng.lib"
+rem rm "zlib-1.2.8/zdll.lib"
+rem cp "libpng1616/projects/vstudio/Debug Library/libpng16.lib" "libpng1616/libpng.lib" 
+rem cp "zlib-1.2.8/build/Debug/zlibstaticd.lib" "zlib-1.2.8/zdll.lib"
+ 
+set CAIROROOT=C:\Develop\code\iwallee\teedow\3rd\cairo
+set "INCLUDE=%INCLUDE%;%CAIROROOT%/src;%CAIROROOT%/pixman/pixman;%CAIROROOT%/zlib-1.2.8;%CAIROROOT%/libpng1616;"
+
+cd pixman
+make -f Makefile.win32 "CFG=debug" "MMX=on" "SSE2=on" "SSSE3=on" 
+
+cd ..
+make -f Makefile.win32 "CFG=debug" "LIBPNG_PATH=%CAIROROOT%/libpng1616" "ZLIB_PATH=%CAIROROOT%/zlib-1.2.8" "PIXMAN_PATH=%CAIROROOT%/pixman" 
