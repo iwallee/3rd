e19062a067fb05046a6de5237d0b57578d3fda00
houbaoshan <houbaoshan@iwallee.com>
2015-02-03 15:54:04 +0800

    m

--- cairo/build-release.sh
+++ cairo/build-release.sh
@@ -1,9 +1,9 @@
-rem rm "libpng1616/libpng.lib"
-rem rm "zlib-1.2.8/zdll.lib"
-rem cp "libpng1616/projects/vstudio/Release Library/libpng16.lib" "libpng1616/libpng.lib" 
-rem cp "zlib-1.2.8/build/Release/zlibstatic.lib" "zlib-1.2.8/zdll.lib"
+# rm "libpng1616/libpng.lib"
+# rm "zlib-1.2.8/zdll.lib"
+# cp "libpng1616/projects/vstudio/Release Library/libpng16.lib" "libpng1616/libpng.lib" 
+# cp "zlib-1.2.8/build/Release/zlibstatic.lib" "zlib-1.2.8/zdll.lib"
  
-set CAIROROOT=C:\Develop\code\iwallee\teedow\3rd\cairo
+set CAIROROOT=%cd%
 set "INCLUDE=%INCLUDE%;%CAIROROOT%/src;%CAIROROOT%/pixman/pixman;%CAIROROOT%/zlib-1.2.8;%CAIROROOT%/libpng1616;"
 
 cd pixman
